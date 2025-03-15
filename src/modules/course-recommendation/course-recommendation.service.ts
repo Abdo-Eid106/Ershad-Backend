c454d0689cba84fd693e3bdae26b208214889d90
Abdo Eid <abdoeid2121@gmail.com>
2025-03-15 15:56:18 +0200

    change recommendation rules

--- src/modules/course-recommendation/course-recommendation.service.ts
+++ src/modules/course-recommendation/course-recommendation.service.ts
@@ -31,42 +31,79 @@ export class CourseRecommendationService {
     const courseIds =
       await this.academicInfoService.getTakenCourseIds(studentId);
 
-    const coursesSubQuery = this.planRepo
+    return this.planRepo
       .createQueryBuilder('plan')
       .leftJoin('plan.semesterPlans', 'semesterPlan')
       .leftJoin('semesterPlan.semesterPlanCourses', 'semesterPlanCourse')
       .innerJoin('semesterPlanCourse.course', 'course')
       .leftJoin('course.prerequisite', 'prerequisite')
-      .andWhere(
-        'prerequisite.id IS NULL OR prerequisite.id IN (:...courseIds)',
-        { courseIds: courseIds.length ? courseIds : [null] },
-      )
-      .andWhere('course.id NOT IN (:...courseIds)', {
+      .where('course.id NOT IN (:...courseIds)', {
         courseIds: courseIds.length ? courseIds : [null],
       })
-      .select([
-        ...COURSE_SELECT_FIELDS,
-        'semesterPlan.level AS level',
-        'semesterPlan.semester AS semester',
-        `SUM(course.creditHours) OVER (
-          ORDER BY semesterPlan.level ASC,  semesterPlan.semester ASC
-        ) AS sum`,
-        '(semesterPlan.level - 1) * 2 + semesterPlan.semester AS semesterOrder',
-      ])
-      .orderBy('semesterOrder', 'ASC');
-
-    const { max: maxCredits } =
-      await this.academicInfoService.getRegistrationHoursRange(studentId);
-    const currentSemester =
-      await this.academicInfoService.getCurrentSemester(studentId);
-
-    return this.entityManager
-      .createQueryBuilder()
-      .select('*')
-      .from(`(${coursesSubQuery.getQuery()})`, 'sub')
-      .where('sub.sum <= :maxCredits', { maxCredits })
-      .andWhere('sub.semesterOrder <= :currentSemester', { currentSemester })
-      .setParameters(coursesSubQuery.getParameters())
+      .andWhere(
+        '(prerequisite.id IS NULL OR prerequisite.id IN (:...courseIds))',
+        {
+          courseIds: courseIds.length ? courseIds : [null],
+        },
+      )
+      .select([...COURSE_SELECT_FIELDS])
+      .orderBy('semesterPlan.level', 'ASC')
+      .addOrderBy('semesterPlan.semester', 'ASC')
       .getRawMany();
   }
+
+  // async getRecommenedCourses(studentId: UUID) {
+  //   const plan =
+  //     (await this.planService.getProgramPlan(studentId)) ||
+  //     (await this.planService.getAlternativeProgramPlan(studentId));
+  //   if (!plan) {
+  //     throw new ServiceUnavailableException(
+  //       'recommendation service is currently unavailable',
+  //     );
+  //   }
+
+  //   const courseIds =
+  //     await this.academicInfoService.getTakenCourseIds(studentId);
+
+  //   const coursesSubQuery = this.planRepo
+  //     .createQueryBuilder('plan')
+  //     .leftJoin('plan.semesterPlans', 'semesterPlan')
+  //     .leftJoin('semesterPlan.semesterPlanCourses', 'semesterPlanCourse')
+  //     .innerJoin('semesterPlanCourse.course', 'course')
+  //     .leftJoin('course.prerequisite', 'prerequisite')
+  //     .where('course.id NOT IN (:...courseIds)', {
+  //       courseIds: courseIds.length ? courseIds : [null],
+  //     })
+  //     .andWhere(
+  //       '(prerequisite.id IS NULL OR prerequisite.id IN (:...courseIds))',
+  //       {
+  //         courseIds: courseIds.length ? courseIds : [null],
+  //       },
+  //     )
+  //     .select([
+  //       ...COURSE_SELECT_FIELDS,
+  //       'semesterPlan.level AS level',
+  //       'semesterPlan.semester AS semester',
+  //       `SUM(course.creditHours) OVER (
+  //         ORDER BY semesterPlan.level ASC,  semesterPlan.semester ASC
+  //       ) AS sum`,
+  //       '(semesterPlan.level - 1) * 2 + semesterPlan.semester AS semesterOrder',
+  //     ])
+  //     .orderBy('semesterPlan.level', 'ASC')
+  //     .addOrderBy('semesterPlan.semester', 'ASC');
+
+  //   const { max: maxCredits } =
+  //     await this.academicInfoService.getRegistrationHoursRange(studentId);
+  //   const currentSemester =
+  //     await this.academicInfoService.getCurrentSemester(studentId);
+
+  //   return this.entityManager
+  //     .createQueryBuilder()
+  //     .select('*')
+  //     .from(`(${coursesSubQuery.getQuery()})`, 'sub')
+  //     .where('sub.sum <= :maxCredits', { maxCredits })
+  //     .andWhere('sub.semesterOrder <= :currentSemester', { currentSemester })
+  //     .setParameters(coursesSubQuery.getParameters())
+  //     .getRawMany();
+  // }
 }
