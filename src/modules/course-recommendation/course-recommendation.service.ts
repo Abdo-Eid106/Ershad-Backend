090d144d34fa045c82ed995accd34901377c2175
Abdo Eid <abdoeid2121@gmail.com>
2025-05-21 02:35:06 +0300

    refactor CourseRecommendationService to simplify course query logic and remove unused code

--- src/modules/course-recommendation/course-recommendation.service.ts
+++ src/modules/course-recommendation/course-recommendation.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, ServiceUnavailableException } from '@nestjs/common';
 import { UUID } from 'crypto';
-import { EntityManager, Repository } from 'typeorm';
-import { AcademicInfo } from '../academic-info/entities/academic-info.entity';
+import { Repository } from 'typeorm';
 import { Plan } from '../plan/entities/plan.entity';
 import { AcademicInfoService } from '../academic-info/academic-info.service';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -15,13 +14,13 @@ export class CourseRecommendationService {
     private readonly planRepo: Repository<Plan>,
     private readonly planService: PlanService,
     private readonly academicInfoService: AcademicInfoService,
-    private readonly entityManager: EntityManager,
   ) {}
 
   async getRecommenedCourses(studentId: UUID) {
     const plan =
       (await this.planService.getProgramPlan(studentId)) ||
       (await this.planService.getAlternativeProgramPlan(studentId));
+
     if (!plan) {
       throw new ServiceUnavailableException(
         'recommendation service is currently unavailable',
@@ -35,9 +34,10 @@ export class CourseRecommendationService {
       .createQueryBuilder('plan')
       .leftJoin('plan.semesterPlans', 'semesterPlan')
       .leftJoin('semesterPlan.semesterPlanCourses', 'semesterPlanCourse')
-      .innerJoin('semesterPlanCourse.course', 'course')
+      .leftJoin('semesterPlanCourse.course', 'course')
       .leftJoin('course.prerequisite', 'prerequisite')
-      .where('course.id NOT IN (:...courseIds)', {
+      .where('plan.programId = :programId', { programId: plan.programId })
+      .andWhere('course.id NOT IN (:...courseIds)', {
         courseIds: courseIds.length ? courseIds : [null],
       })
       .andWhere(
@@ -51,59 +51,4 @@ export class CourseRecommendationService {
       .addOrderBy('semesterPlan.semester', 'ASC')
       .getRawMany();
   }
-
-  // async getRecommenedCourses(studentId: UUID) {
-  //   const plan =
-  //     (await this.planService.getProgramPlan(studentId)) ||
-  //     (await this.planService.getAlternativeProgramPlan(studentId));
-  //   if (!plan) {
-  //     throw new ServiceUnavailableException(
-  //       'recommendation service is currently unavailable',
-  //     );
-  //   }
-
-  //   const courseIds =
-  //     await this.academicInfoService.getTakenCourseIds(studentId);
-
-  //   const coursesSubQuery = this.planRepo
-  //     .createQueryBuilder('plan')
-  //     .leftJoin('plan.semesterPlans', 'semesterPlan')
-  //     .leftJoin('semesterPlan.semesterPlanCourses', 'semesterPlanCourse')
-  //     .innerJoin('semesterPlanCourse.course', 'course')
-  //     .leftJoin('course.prerequisite', 'prerequisite')
-  //     .where('course.id NOT IN (:...courseIds)', {
-  //       courseIds: courseIds.length ? courseIds : [null],
-  //     })
-  //     .andWhere(
-  //       '(prerequisite.id IS NULL OR prerequisite.id IN (:...courseIds))',
-  //       {
-  //         courseIds: courseIds.length ? courseIds : [null],
-  //       },
-  //     )
-  //     .select([
-  //       ...COURSE_SELECT_FIELDS,
-  //       'semesterPlan.level AS level',
-  //       'semesterPlan.semester AS semester',
-  //       `SUM(course.creditHours) OVER (
-  //         ORDER BY semesterPlan.level ASC,  semesterPlan.semester ASC
-  //       ) AS sum`,
-  //       '(semesterPlan.level - 1) * 2 + semesterPlan.semester AS semesterOrder',
-  //     ])
-  //     .orderBy('semesterPlan.level', 'ASC')
-  //     .addOrderBy('semesterPlan.semester', 'ASC');
-
-  //   const { max: maxCredits } =
-  //     await this.academicInfoService.getRegistrationHoursRange(studentId);
-  //   const currentSemester =
-  //     await this.academicInfoService.getCurrentSemester(studentId);
-
-  //   return this.entityManager
-  //     .createQueryBuilder()
-  //     .select('*')
-  //     .from(`(${coursesSubQuery.getQuery()})`, 'sub')
-  //     .where('sub.sum <= :maxCredits', { maxCredits })
-  //     .andWhere('sub.semesterOrder <= :currentSemester', { currentSemester })
-  //     .setParameters(coursesSubQuery.getParameters())
-  //     .getRawMany();
-  // }
 }
