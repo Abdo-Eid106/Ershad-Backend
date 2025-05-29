e0ca444ab17c20fb6fc4e3bb2aa64dc8cae5d2bd
Abdo Eid <abdoeid2121@gmail.com>
2025-05-29 19:46:33 +0300

    refactor Requirement module to update validation messages in CreateRequirementDto and introduce new error enums for course and program IDs

--- src/modules/requirement/dto/create-requirement.dto.ts
+++ src/modules/requirement/dto/create-requirement.dto.ts
@@ -4,21 +4,21 @@ import { UUID } from 'crypto';
 import { ErrorEnum } from 'src/shared/i18n/enums/error.enum';
 
 export class CreateRequirementDto {
-  @IsUUID('4', { message: ErrorEnum.REQUIREMENT_COURSE_ID_UUID })
+  @IsUUID('4', { message: ErrorEnum.COURSE_ID_UUID })
   courseId: UUID;
 
-  @IsUUID('4', { message: ErrorEnum.REQUIREMENT_REGULATION_ID_UUID })
+  @IsUUID('4', { message: ErrorEnum.REGULATION_ID_UUID })
   regulationId: UUID;
 
-  @IsBoolean({ message: ErrorEnum.REQUIREMENT_OPTIONAL_BOOLEAN })
+  @IsBoolean({ message: ErrorEnum.OPTIONAL_BOOLEAN })
   optional: boolean;
 
   @IsIn(Object.values(RequirementCategory), {
-    message: ErrorEnum.REQUIREMENT_CATEGORY_IN,
+    message: ErrorEnum.CATEGORY_IN,
   })
   category: RequirementCategory;
 
   @ValidateIf((o) => o.type == RequirementCategory.SPECIALIZATION)
-  @IsUUID('4', { message: ErrorEnum.REQUIREMENT_PROGRAM_ID_UUID })
+  @IsUUID('4', { message: ErrorEnum.PROGRAM_ID_UUID })
   programId?: UUID;
 }
