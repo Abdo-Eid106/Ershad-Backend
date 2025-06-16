a1db062306d4ab1e1254074a4bdb8256ef067bb0
Abdo Eid <abdoeid2121@gmail.com>
2025-06-16 06:06:19 +0300

    feat: add CreateFcmToken DTO for FCM token management and update import path in controller

--- src/modules/fcm-token/fcm-token.controller.ts
+++ src/modules/fcm-token/fcm-token.controller.ts
@@ -10,7 +10,7 @@ import { FcmTokenService } from './fcm-token.service';
 import { JwtGuard } from '../auth/guards/jwt.guard';
 import { currentUser } from 'src/shared/decorators/current-user.decorator';
 import { IPayloud } from 'src/shared/interfaces/payloud.interface';
-import { CreateFcmToken } from './enums/create-fcm-token.dto';
+import { CreateFcmToken } from './dto/create-fcm-token.dto';
 
 @Controller('fcm-tokens')
 @UseGuards(JwtGuard)
