fbd44c261e50f40672160d139d80b78547fd97d4
Abdo Eid <abdoeid2121@gmail.com>
2025-06-19 01:05:48 +0300

    refactor: remove unused SemestersDto class and update exports

--- src/modules/semester/dto/index.ts
+++ src/modules/semester/dto/index.ts
@@ -1,4 +1,3 @@
 export { CreateSemesterDto } from './input/create-semester.dto';
 export { UpdateSemesterDto } from './input/update-semester.dto';
-export { SemestersDto } from './output/semesters.dto';
 export { SemesterDto } from './output/semester.dto';
