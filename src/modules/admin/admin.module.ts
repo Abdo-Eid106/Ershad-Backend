3e00e3be5569baa1c7adab139be3d2617ba635f5
Abdo Eid <abdoeid2121@gmail.com>
2025-06-19 04:55:07 +0300

    refactor: remove AdminController and related DTOs from Admin module

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
-import { AdminController } from './admin.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Admin } from './entities/admin.entity';
 import { User } from '../user/entities/user.entity';
@@ -8,7 +7,6 @@ import { Role } from '../auth/entities/role.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Admin, User, Role])],
-  controllers: [AdminController],
   providers: [AdminService],
 })
 export class AdminModule {}
--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,13 +1,8 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Admin } from './entities/admin.entity';
 import { Repository } from 'typeorm';
-import { CreateAdminDto } from './dto/create-admin.dto';
 import { User } from '../user/entities/user.entity';
-import {
-  ConflictException,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { OnModuleInit } from '@nestjs/common';
 import { hash } from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
 import { Role } from '../auth/entities/role.entity';
@@ -24,16 +19,24 @@ export class AdminService implements OnModuleInit {
     private readonly configService: ConfigService,
   ) {}
 
-  async create(createAdminDto: CreateAdminDto) {
-    const { email } = createAdminDto;
+  async onModuleInit() {
+    const email = this.configService.get<string>('ADMIN_EMAIL');
+    let password = this.configService.get<string>('ADMIN_PASSWORD');
 
-    if (await this.userRepo.existsBy({ email }))
-      throw new ConflictException('email in use');
+    const adminExist = await this.userRepo.existsBy({ email });
+    if (adminExist) return;
 
-    const password = await hash(createAdminDto.password, 12);
-    const role = await this.roleRepo.findOne({
+    password = await hash(password, 12);
+
+    let role = await this.roleRepo.findOne({
       where: { name: RoleEnum.ADMIN },
     });
+    if (!role) {
+      role = await this.roleRepo.save(
+        this.roleRepo.create({ name: RoleEnum.ADMIN }),
+      );
+    }
+
     const user = await this.userRepo.save(
       this.userRepo.create({
         email,
@@ -43,34 +46,7 @@ export class AdminService implements OnModuleInit {
     );
 
     return this.adminRepo.save(
-      this.adminRepo.create({ user, name: createAdminDto.name }),
+      this.adminRepo.create({ user, name: { en: 'admin', ar: 'ادمن' } }),
     );
-  }
-
-  async findAll() {
-    return this.adminRepo
-      .createQueryBuilder('Admin')
-      .innerJoin('Admin.user', 'user')
-      .select(['user.id AS id', 'user.email AS email', 'Admin.name AS name'])
-      .getRawMany();
-  }
-
-  async remove(id: User['id']) {
-    const user = await this.userRepo.findOne({ where: { id } });
-    if (!user) throw new NotFoundException('Admin not found');
-    return this.userRepo.remove(user);
-  }
-
-  async onModuleInit() {
-    const email = this.configService.get<string>('ADMIN_EMAIL');
-    const password = this.configService.get<string>('ADMIN_PASSWORD');
-
-    if (!(await this.userRepo.existsBy({ email }))) {
-      await this.create({
-        email,
-        password,
-        name: { en: '', ar: '' },
-      });
-    }
   }
 }
