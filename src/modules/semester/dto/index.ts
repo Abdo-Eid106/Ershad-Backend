b09e08c30071ecdfa468cb394a9d48da03093fb7
Abdo Eid <abdoeid2121@gmail.com>
2025-06-23 15:53:34 +0300

    refactor: remove UpdateSemesterDto export for simplification

--- src/modules/semester/dto/index.ts
+++ src/modules/semester/dto/index.ts
@@ -1,3 +1,2 @@
 export { CreateSemesterDto } from './input/create-semester.dto';
-export { UpdateSemesterDto } from './input/update-semester.dto';
 export { SemesterDto } from './output/semester.dto';
